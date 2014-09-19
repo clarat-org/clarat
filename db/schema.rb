c590b38ab6cc4ac85a0079ae3194f5753dd238b9
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2014-09-19 11:15:31 +0200

    Add name to location

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140917143354) do
+ActiveRecord::Schema.define(version: 20140919091220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20140917143354) do
     t.integer  "federal_state_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   create_table "offers", force: true do |t|

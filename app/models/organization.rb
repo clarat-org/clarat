dfa7507b273fde9c1fe6aec53649c87b837f5417
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2014-09-19 11:46:59 +0200

    Add clasification to organizations and make it optional

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -10,12 +10,15 @@ class Organization < ActiveRecord::Base
   # Enumerization
   extend Enumerize
   enumerize :legal_form, in: %w[ev ggmbh gag foundation gug kdor gmbh ag ug]
-  enumerize :classification, in: %w[welfare other]
+  enumerize :classification, in: ['Caritas', 'Diakonie', 'Arbeiterwohlfahrt',
+                                  'Deutscher Paritätischer Wohlfahrtsverband',
+                                  'Deutsches Rotes Kreuz',
+                                  'Zentralwohlfahrtsstelle der Juden in
+                                   Deutschland']
 
   # Validations
   validates :name, length: { maximum: 100 }, presence: true
   validates :description, length: { maximum: 400 }, presence: true
   validates :legal_form, presence: true
-  validates :classification, presence: true
   validates :founded, length: { is: 4 }
 end

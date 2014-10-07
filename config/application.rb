b44fcea746bc1e976fdcf0271913f99a0df5a2a5
Konstantin K <konstantin.koss@gmail.com>
2014-10-07 13:33:17 +0200

    hopefully working precompilation

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,16 @@ module Clarat
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    config.assets.initialize_on_precompile = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
     # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
     config.assets.precompile += Dir["app/assets/stylesheets/controller/*.scss"].map{|file| "controller/#{File.basename file,'.scss'}" }
     config.assets.precompile += %w( gmaps_search_results.js )
+    config.generators.assets :controller_based_assets
   end
 end

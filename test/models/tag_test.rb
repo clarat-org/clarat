47b6b054c1ae58003d3237be15c825a3bea03f51
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2014-10-21 16:53:45 +0200

    Add tag model tests

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,31 @@
-require 'test_helper'
+require_relative '../test_helper'
 
-class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe Tag do
+
+  let(:tag) { Tag.new }
+
+  subject { tag }
+
+  describe 'attributes' do
+    it { subject.must_respond_to :id }
+    it { subject.must_respond_to :name }
+    it { subject.must_respond_to :main }
+    it { subject.must_respond_to :created_at }
+    it { subject.must_respond_to :updated_at }
+  end
+
+  describe 'validations' do
+    describe 'always' do
+      it { subject.must validate_presence_of :name }
+      it { subject.must validate_uniqueness_of(:name) }
+    end
+  end
+
+  describe '::Base' do
+    describe 'associations' do
+      it { subject.must have_and_belong_to_many :associated_tags }
+      it { subject.must have_and_belong_to_many :offers }
+      it { subject.must have_many :organizations }
+    end
+  end
 end

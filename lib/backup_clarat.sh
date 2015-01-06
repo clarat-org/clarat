7fb5c597c265deee6d7e07d314713063aa3cd02c
Konstantin K <konstantin.koss@gmail.com>
2015-01-06 16:10:20 +0100

    for #77 - script doesn't need cd anymore

--- lib/backup_clarat.sh
+++ lib/backup_clarat.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-cd `dirname $0`
-cd ..
 /app/vendor/heroku-toolbelt/bin/heroku pgbackups:capture --app clarat
 export CURRENT_TIME=`date +"%Y-%m-%d-%H-%M-%S"`
-curl -o /tmp/clarat-$CURRENT_TIME.dump `/app/vendor/heroku-toolbelt/bin/heroku pgbackups:url`
+curl -o /tmp/clarat-$CURRENT_TIME.dump `/app/vendor/heroku-toolbelt/bin/heroku pgbackups:url --app clarat`
 sftp -oPort=22022 sftp@$SFTP_IP:Backup-DB <<COMMAND
   put /tmp/clarat-$CURRENT_TIME.dump
   quit

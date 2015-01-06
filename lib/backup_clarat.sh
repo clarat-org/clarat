bcd723956a4d4074d2261c8b7a71c963f2a86397
Konstantin K <konstantin.koss@gmail.com>
2015-01-06 15:42:07 +0100

    for #77 - heroku-toolbelt needs specific app on heroku

--- lib/backup_clarat.sh
+++ lib/backup_clarat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd `dirname $0`
 cd ..
-/app/vendor/heroku-toolbelt/bin/heroku pgbackups:capture
+/app/vendor/heroku-toolbelt/bin/heroku pgbackups:capture --app clarat
 export CURRENT_TIME=`date +"%Y-%m-%d-%H-%M-%S"`
 curl -o /tmp/clarat-$CURRENT_TIME.dump `/app/vendor/heroku-toolbelt/bin/heroku pgbackups:url`
 sftp -oPort=22022 sftp@$SFTP_IP:Backup-DB <<COMMAND

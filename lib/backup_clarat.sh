c9d922842ae48c7321fdcd9c01a06d1e134de372
Konstantin K <konstantin.koss@gmail.com>
2015-01-06 15:37:52 +0100

    for #77 - correct heroku bin path

--- lib/backup_clarat.sh
+++ lib/backup_clarat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd `dirname $0`
 cd ..
-heroku pgbackups:capture
+/app/vendor/heroku-toolbelt/bin/heroku pgbackups:capture
 export CURRENT_TIME=`date +"%Y-%m-%d-%H-%M-%S"`
-curl -o /tmp/clarat-$CURRENT_TIME.dump `heroku pgbackups:url`
+curl -o /tmp/clarat-$CURRENT_TIME.dump `/app/vendor/heroku-toolbelt/bin/heroku pgbackups:url`
 sftp -oPort=22022 sftp@$SFTP_IP:Backup-DB <<COMMAND
   put /tmp/clarat-$CURRENT_TIME.dump
   quit

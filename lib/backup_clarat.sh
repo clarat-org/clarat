b5d336b2710f60351fa5dc383f0347a14d8980c8
Konstantin K <konstantin.koss@gmail.com>
2015-01-06 14:52:21 +0100

    for #77 - expand bash script to be usable from heroku@

--- lib/backup_clarat.sh
+++ lib/backup_clarat.sh
@@ -2,4 +2,9 @@
 cd `dirname $0`
 cd ..
 heroku pgbackups:capture
-curl -o clarat-`date +"%Y-%m-%d-%H-%M-%S"`.dump `heroku pgbackups:url`
+export CURRENT_TIME=`date +"%Y-%m-%d-%H-%M-%S"`
+curl -o /tmp/clarat-$CURRENT_TIME.dump `heroku pgbackups:url`
+sftp -oPort=22022 sftp@$SFTP_IP:Backup-DB <<COMMAND
+  put /tmp/clarat-$CURRENT_TIME.dump
+  quit
+COMMAND

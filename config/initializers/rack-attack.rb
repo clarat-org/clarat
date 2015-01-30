e7f6726a3da384f6756ebf409c035a482e7c64de
Konstantin K <konstantin.koss@gmail.com>
2015-01-30 13:54:53 +0100

    anonymize stored IPs

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,11 +1,16 @@
 class Rack::Attack
-  Rack::Attack.throttle('location_api_requests/ip', limit: 20, period: 1.minute) do |req|
-    req.ip if req.path.match /\A\/search_locations\/.*\z/
+  Rack::Attack.throttle(
+    'location_api_requests/ip',
+    limit: 20,
+    period: 1.minute
+  ) do |req|
+    next unless req.path.match /\A\/search_locations\/.*\z/
+    Digest::SHA1.hexdigest(req.ip) # anonymize IP
   end
 
   # Limit DOS?
   # Careful: each asset request also counts towards the limit
   # Rack::Attack.throttle('req/ip', limit: 30, period: 30.seconds) do |req|
-  #   req.ip
+  #   Digest::SHA1.hexdigest(req.ip)
   # end
 end

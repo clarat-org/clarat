d44af3fe86eec7ea17e00b79091487390254e16f
Konstantin K <konstantin.koss@gmail.com>
2015-02-12 16:17:10 +0100

    for #134 - index offers with all ancestor tags

--- db/migrate/20150212133049_remove_dependent_categories.rb
+++ db/migrate/20150212133049_remove_dependent_categories.rb
@@ -1,6 +1,8 @@
 class RemoveDependentCategories < ActiveRecord::Migration
-  def up
+  def change
     drop_table :dependent_categories
     drop_table :dependent_tags
+
+    Offer.reindex! # I just didn't want to write a new migration for this ...
   end
 end

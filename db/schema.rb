3b9dbf3b491e754d83d82abd410297721ce058e8
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2015-02-12 17:40:05 +0100

    Refactoring

--- db/schema.rb
+++ db/schema.rb
@@ -140,14 +140,6 @@ ActiveRecord::Schema.define(version: 20150212133049) do
   add_index "offers_openings", ["offer_id"], name: "index_offers_openings_on_offer_id", using: :btree
   add_index "offers_openings", ["opening_id"], name: "index_offers_openings_on_opening_id", using: :btree
 
-  create_table "offers_tags", id: false, force: true do |t|
-    t.integer "offer_id", null: false
-    t.integer "tag_id",   null: false
-  end
-
-  add_index "offers_tags", ["offer_id"], name: "index_offers_tags_on_offer_id", using: :btree
-  add_index "offers_tags", ["tag_id"], name: "index_offers_tags_on_tag_id", using: :btree
-
   create_table "openings", force: true do |t|
     t.string   "day",        limit: 3, null: false
     t.time     "open"
@@ -209,17 +201,6 @@ ActiveRecord::Schema.define(version: 20150212133049) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-  create_table "tags", force: true do |t|
-    t.string   "name",                                  null: false
-    t.boolean  "main",                  default: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "icon",       limit: 12
-    t.text     "synonyms"
-  end
-
-  add_index "tags", ["name"], name: "index_tags_on_name", using: :btree
 
   create_table "update_requests", force: true do |t|
     t.string   "search_location", null: false

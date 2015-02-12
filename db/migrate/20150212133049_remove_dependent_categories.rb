585307b4a0387f1eb81c33788ddf3c0e3c46dfef
Konstantin K <konstantin.koss@gmail.com>
2015-02-12 16:21:28 +0100

    for #134 - migration mistake

--- db/migrate/20150212133049_remove_dependent_categories.rb
+++ db/migrate/20150212133049_remove_dependent_categories.rb
@@ -1,7 +1,6 @@
 class RemoveDependentCategories < ActiveRecord::Migration
   def change
     drop_table :dependent_categories
-    drop_table :dependent_tags
 
     Offer.reindex! # I just didn't want to write a new migration for this ...
   end

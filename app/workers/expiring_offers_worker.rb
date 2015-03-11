5eb34bf455a6e29a0f05eb1f25cdf59b11b98ab2
Konstantin K <konstantin.koss@gmail.com>
2015-03-11 15:31:16 +0100

    remove travis debugging puts and hopefully fix tests

--- app/workers/expiring_offers_worker.rb
+++ app/workers/expiring_offers_worker.rb
@@ -5,24 +5,8 @@ class ExpiringOffersWorker
   recurrence { daily.hour_of_day(1) }
 
   def perform
-    puts Time.now.beginning_of_day
-    puts Time.now.end_of_day
-    expiring =
-      Offer.where(
-        expires_at: Date.current
-      )
-
+    expiring = Offer.where(expires_at: Date.current)
     OfferMailer.delay.expiring_mail expiring.count, expiring.pluck(:id)
-    puts '2:'
-    o = Offer.find(2)
-    if o
-      puts o.expires_at
-      puts o.expires_at >= Time.now.beginning_of_day
-      puts o.expires_at <= Time.now.end_of_day
-
-    end
-    puts 'unapproving:'
-    expiring.find_each { |offer| pp offer.attributes }
     expiring.update_all approved: false
   end
 end

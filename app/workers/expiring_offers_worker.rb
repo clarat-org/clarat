368811224926a33b0e7952e8f2d7d666e608c691
Konstantin K <konstantin.koss@gmail.com>
2015-03-11 15:19:12 +0100

    temp changes to debug travis 4

--- app/workers/expiring_offers_worker.rb
+++ app/workers/expiring_offers_worker.rb
@@ -9,14 +9,18 @@ class ExpiringOffersWorker
     puts Time.now.end_of_day
     expiring =
       Offer.where(
-        'expires_at >= ?', Time.now.beginning_of_day
-      ).where(
-        'expires_at <= ?', Time.now.end_of_day
+        expires_at: Date.current
       )
 
     OfferMailer.delay.expiring_mail expiring.count, expiring.pluck(:id)
-    puts 'all:'
-    Offer.all.each { |offer| pp offer.attributes }
+    puts '2:'
+    o = Offer.find(2)
+    if o
+      puts o.expires_at
+      puts o.expires_at >= Time.now.beginning_of_day
+      puts o.expires_at <= Time.now.end_of_day
+
+    end
     puts 'unapproving:'
     expiring.find_each { |offer| pp offer.attributes }
     expiring.update_all approved: false

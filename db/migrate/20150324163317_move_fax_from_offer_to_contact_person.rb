868a5b3e2426ef6e48560ffeaa468826e710607e
Sebastian Schwiecker <sebastian.schwiecker@gmail.com>
2015-03-24 17:44:59 +0100

    Add migration to move fax from offers to contact_people

---
+++ db/migrate/20150324163317_move_fax_from_offer_to_contact_person.rb
@@ -0,0 +1,14 @@
+class MoveFaxFromOfferToContactPerson < ActiveRecord::Migration
+  def change
+    add_column :contact_people, :fax_area_code, :string, limit: 6
+    add_column :contact_people, :fax_number, :string, limit: 32
+
+    Offer.find_each do |offer|
+      cp = ContactPerson.new(fax_number: offer.fax)
+      cp.save(validate: false)
+      offer.contact_people << cp
+    end
+
+    remove_column :offers, :fax, :string
+  end
+end

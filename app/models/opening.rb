8832b6a9e909343b44b59c13dbbb2323d20aa198
Konstantin K <konstantin.koss@gmail.com>
2015-06-10 12:03:39 +0200

    finish #306 - fix Opening#display_string bug when close=nil

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -40,11 +40,17 @@ class Opening < ActiveRecord::Base
     !open && !close
   end
 
+  # The way an opening time frame is displayed to the user in the front end.
   def display_string
-    if close.strftime('%H:%M') == '00:00'
-      "#{open.strftime('%H:%M')} Uhr - 24:00 Uhr"
+    if !close # there can also be no opening, because of validations
+      I18n.t 'opening.display_string.appointment'
+
+    elsif close.strftime('%H:%M') == '00:00'
+      I18n.t 'opening.display_string.time_frame', open: open.strftime('%H:%M'),
+                                                  close: '24:00'
     else
-      "#{open.strftime('%H:%M')} Uhr - #{close.strftime('%H:%M')} Uhr"
+      I18n.t 'opening.display_string.time_frame', open: open.strftime('%H:%M'),
+                                                  close: close.strftime('%H:%M')
     end
   end
 

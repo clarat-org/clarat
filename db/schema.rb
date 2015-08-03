54b7c5ae24dd2637f7a104641d415d14b0c5f553
Konstantin K <konstantin.koss@gmail.com>
2015-08-03 12:10:36 +0200

    for testing of #379 - give migration the latest timestamp

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724152358) do
+ActiveRecord::Schema.define(version: 20150803100838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"

6beae774b99a2a810c6a804ef4c0918fa306ee01
Konstantin K <konstantin.koss@gmail.com>
2015-08-19 12:00:39 +0200

    fix for error recovery in expiring workers - expire (approved) offers that expired today *or were missed beore*

--- app/workers/expiring_offers_worker.rb
+++ app/workers/expiring_offers_worker.rb
@@ -5,7 +5,7 @@ class ExpiringOffersWorker
   recurrence { daily.hour_of_day(3) }
 
   def perform
-    expiring = Offer.where(expires_at: Time.zone.today)
+    expiring = Offer.approved.where('expires_at <= ?', Time.zone.today)
     if expiring.count > 0
       OfferMailer.delay.expiring_mail expiring.count, expiring.pluck(:id)
       expiring.update_all approved: false

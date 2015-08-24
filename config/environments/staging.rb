8eeb7616f7a3bc71641e2a67bf7b8d3123e31d37
Konstantin K <konstantin.koss@gmail.com>
2015-08-24 17:01:15 +0200

    revert allowing mailing deliveries on staging for testing

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -110,6 +110,6 @@ Clarat::Application.configure do
   config.action_mailer.default_url_options = { host: 'staging.clarat.org' }
   config.action_mailer.delivery_method = :smtp
   # Keep the following on FALSE unless you are explicitly testing mailings:
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.raise_delivery_errors = true
 end

d3e8a93756dbe1cdf3b1dd8300bd9af8c0fdd08a
Konstantin K <konstantin.koss@gmail.com>
2015-09-03 11:24:45 +0200

    for #437 - enable deactivation of organizations

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -67,6 +67,10 @@ class Organization < ActiveRecord::Base
     state :completed
     state :approved
 
+    # Special states object might enter after it was approved
+    state :internal_feedback # There was an issue (internal)
+    state :external_feedback # There was an issue (external)
+
     ## Transitions
 
     event :complete do
@@ -75,6 +79,18 @@ class Organization < ActiveRecord::Base
 
     event :approve, before: :set_approved_information do
       transitions from: :completed, to: :approved, guard: :different_actor?
+      transitions from: :internal_feedback, to: :approved
+      transitions from: :external_feedback, to: :approved
+    end
+
+    event :deactivate_internal do
+      transitions from: :approved, to: :internal_feedback
+      transitions from: :external_feedback, to: :internal_feedback
+    end
+
+    event :deactivate_external do
+      transitions from: :approved, to: :external_feedback
+      transitions from: :internal_feedback, to: :external_feedback
     end
   end
 

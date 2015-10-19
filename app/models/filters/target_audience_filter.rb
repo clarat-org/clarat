c433490af356d5320e89d8f36f8756fd17aa03c4
Nils Vollmer <nils_vollmer@gmx.de>
2015-10-19 09:17:54 +0200

    changed targetAudienceFilter identifiers to match algolia content

--- app/models/filters/target_audience_filter.rb
+++ app/models/filters/target_audience_filter.rb
@@ -1,5 +1,5 @@
 class TargetAudienceFilter < Filter
   validates :identifier, uniqueness: true, presence: true
 
-  IDENTIFIER = %w(children parents family acquintances pregnant_woman)
+  IDENTIFIER = %w(children parents nuclear_family aquintances pregnant_woman)
 end

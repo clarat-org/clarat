1904290c837d4d5abba505d2721e1d79f633b0e2
Nils Vollmer <nils_vollmer@gmx.de>
2015-11-11 14:15:39 +0100

    #449 updated about us section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,8 +4,9 @@ class PagesController < ApplicationController
   end
 
   def about
-    @names = %w(amina andrea anne basti elisa janina jens jule julia julian
-                konstantin lisa marcus nicole stefan verena)
+    @names = %w(amina andrea anne astrid basti bettina elisa franzi janina janne
+                jens jule julia julian katinka konstantin laura lavinia line
+                lisa marcus nicole nils stefan tine verena)
   end
 
   def faq

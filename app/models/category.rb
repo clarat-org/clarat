0ea61e3c2032daecb27cbda37aa7179a73a672a0
Konstantin K <konstantin.koss@gmail.com>
2015-12-07 12:26:37 +0100

    bullshit code for #568 - "schizophrenic categories"

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,4 +11,26 @@ class Category < ActiveRecord::Base
       hash_tree.sort_by { |tree| tree.first.icon || '' }
     end
   end
+
+  # TODO: BEEEEEH! Wiedervorlage spätestens 1. Februar
+  # auch im ApplicationController!
+
+  def self.current_section= section
+    @current_section = section
+  end
+
+  def self.current_section
+    @current_section
+  end
+
+  def name
+    original_name = super
+    if original_name == 'Sorgen im Alltag' &&
+       Category.current_section == 'refugees'
+      return 'Leben in Deutschland'
+    else
+      original_name
+    end
+  end
+  # /BEEEEEH
 end

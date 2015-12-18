9f5b8e6a5559a3a29f91a9a7447d27509a71848e
Nils Vollmer <nils_vollmer@gmx.de>
2015-12-18 16:10:12 +0100

    added time formatting per locale to openings

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -9,12 +9,18 @@ class Opening < ActiveRecord::Base
     if !close # there can also be no opening, because of validations
       I18n.t 'opening.display_string.appointment'
 
-    elsif close.strftime('%H:%M') == '00:00'
-      I18n.t 'opening.display_string.time_frame', open: open.strftime('%H:%M'),
-                                                  close: '24:00'
     else
-      I18n.t 'opening.display_string.time_frame', open: open.strftime('%H:%M'),
-                                                  close: close.strftime('%H:%M')
+      format_string = I18n.t 'opening.display_string.time_format'
+      open_string = I18n.l(open, format: format_string)
+      close_string = I18n.l(close, format: format_string)
+      # midnight exception for de format. Can be expanded to other languages
+      # by checking close.hour and close.minutes and adding content in locales
+      if close_string == '00:00 Uhr'
+        close_string = I18n.t('opening.display_string.midnight_exception')
+      end
+      I18n.t 'opening.display_string.time_frame',
+             open: open_string,
+             close: close_string
     end
   end
 end

c917089737e867a178414271a828841620c5600d
Konstantin K <konstantin.koss@gmail.com>
2016-01-14 17:41:05 +0100

    fix for ##554 - use image_path to display translated-by-google image

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   config.assets.initialize_on_precompile = false
 
   # Version of your assets, change this if you want to expire all your assets
-  config.assets.version = '1.1.3'
+  config.assets.version = '1.1.4'
 
   # Enable fonts directory
   config.assets.paths << Rails.root.join("app", "assets", "fonts")

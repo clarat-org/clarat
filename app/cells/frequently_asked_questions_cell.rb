3e7a14ba15b2104a269012eae24c7076c8ceda01
Nils Vollmer <nils_vollmer@gmx.de>
2016-02-09 16:14:24 +0100

    new FAQ code with cells gem, optimized code for expandability and added new german fab-locales

---
+++ app/cells/frequently_asked_questions_cell.rb
@@ -0,0 +1,55 @@
+class FrequentlyAskedQuestionsCell < Cell::ViewModel
+  include Cell::Slim
+
+  def show
+    @section_count = section_count
+    render
+  end
+
+  def section section_id
+    @section_number = section_id
+    @question_count = question_count section_id
+    render
+  end
+
+  def question section_id, question_id
+    link_options = link_options_hash section_id, question_id
+    @question_text = I18n.t(".section_#{section_id}.question_#{question_id}.question")
+    @answer_text = I18n.t ".section_#{section_id}.question_#{question_id}.answer", link_options
+    render
+  end
+
+  private
+
+  def section_count
+    count = 1
+    while I18n.exists?(".section_#{count}", :de) do
+      count += 1
+    end
+    count
+  end
+
+  def question_count section_id
+    count = 1
+    while I18n.exists?(".section_#{section_id}.question_#{count}", :de) do
+      count += 1
+    end
+    count
+  end
+
+  def link_options_hash index, q_id
+    l_id = 1
+    options_hash = {}
+    while I18n.exists?(".section_#{index}.question_#{q_id}.link_#{l_id}.var", :de) do
+      var = I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.var").to_sym
+      name = I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.name")
+      url = I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.url")
+      target = I18n.exists?(".section_#{index}.question_#{q_id}.link_#{l_id}.target", :de) ? I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.target") : ''
+      klass = I18n.exists?(".section_#{index}.question_#{q_id}.link_#{l_id}.class", :de) ? I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.class") : ''
+      options_hash[var] = link_to(name, url, target: target, class: klass)
+      l_id += 1
+    end
+    options_hash
+  end
+
+end
---
+++ test/cells/frequently_asked_questions_cell_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class FrequentlyAskedQuestionsCellTest < Cell::TestCase
+  test "show" do
+    # html = cell("frequently_asked_questions").(:show)
+    # assert html.match /<p>/
+  end
+
+
+end

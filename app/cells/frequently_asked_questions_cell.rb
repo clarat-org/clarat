ad6054620217455401fe06df49694953ac57c222
Nils Vollmer <nils_vollmer@gmx.de>
2016-02-10 16:54:21 +0100

    added en, changed de and added some feature tests

--- app/cells/frequently_asked_questions_cell.rb
+++ app/cells/frequently_asked_questions_cell.rb
@@ -2,21 +2,22 @@ class FrequentlyAskedQuestionsCell < Cell::ViewModel
   include Cell::Slim
 
   def show
-    @section_count = section_count
+    @section_ids = (1..section_count - 1).to_a
     render
   end
 
   def section section_id
-    @section_number = section_id
-    @question_count = question_count section_id
+    @section_id = section_id
+    @question_ids = (1..question_count(section_id) - 1).to_a
     render
   end
 
-  def question section_id, question_id
-    link_options = link_options_hash section_id, question_id
-    @anchor = I18n.exists?(".section_#{section_id}.question_#{question_id}.anchor", :de) ? I18n.t(".section_#{section_id}.question_#{question_id}.anchor") : ''
-    @question_text = I18n.t(".section_#{section_id}.question_#{question_id}.question")
-    @answer_text = I18n.t ".section_#{section_id}.question_#{question_id}.answer", link_options
+  def question s_id, q_id
+    link_options = link_options_hash s_id, q_id
+    question_hash = I18n.t ".section_#{s_id}.question_#{q_id}"
+    @anchor = question_hash[:anchor] ? question_hash[:anchor] : ''
+    @question = question_hash[:question]
+    @answer = I18n.t ".section_#{s_id}.question_#{q_id}.answer", link_options
     render
   end
 
@@ -24,33 +25,29 @@ class FrequentlyAskedQuestionsCell < Cell::ViewModel
 
   def section_count
     count = 1
-    while I18n.exists?(".section_#{count}", :de) do
-      count += 1
-    end
+    count += 1 while I18n.exists?(".section_#{count}", I18n.locale)
     count
   end
 
   def question_count section_id
     count = 1
-    while I18n.exists?(".section_#{section_id}.question_#{count}", :de) do
-      count += 1
-    end
+    count += 1 while I18n.exists?(".section_#{section_id}.question_#{count}",
+                                  I18n.locale)
     count
   end
 
   def link_options_hash index, q_id
     l_id = 1
     options_hash = {}
-    while I18n.exists?(".section_#{index}.question_#{q_id}.link_#{l_id}.var", :de) do
-      var = I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.var").to_sym
-      name = I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.name")
-      url = I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.url")
-      target = I18n.exists?(".section_#{index}.question_#{q_id}.link_#{l_id}.target", :de) ? I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.target") : ''
-      klass = I18n.exists?(".section_#{index}.question_#{q_id}.link_#{l_id}.class", :de) ? I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}.class") : ''
-      options_hash[var] = link_to(name, url, target: target, class: klass)
+    while I18n.exists?(".section_#{index}.question_#{q_id}.link_#{l_id}.var",
+                       I18n.locale)
+      link_hash = I18n.t(".section_#{index}.question_#{q_id}.link_#{l_id}")
+      target = link_hash[:target] ? link_hash[:target] : ''
+      klass = link_hash[:class] ? link_hash[:class] : ''
+      options_hash[link_hash[:var].to_sym] =
+        link_to(link_hash[:name], link_hash[:url], target: target, class: klass)
       l_id += 1
     end
     options_hash
   end
-
 end

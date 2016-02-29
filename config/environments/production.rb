c5f3618b828b3f4fde0c5180c474cd009e89b487
Konstantin K <konstantin.koss@gmail.com>
2016-02-29 12:21:45 +0100

    Revert "for #705 - replace mandrill with sendgrit"
    This reverts commit a42f38085b517ea750eca6d8b2c82126abf61bdb.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,12 +98,11 @@ Clarat::Application.configure do
 
   # ActionMailer Config
   config.action_mailer.smtp_settings = {
-    address:   "smtp.sendgrid.net",
+    address:   "smtp.mandrillapp.com",
     port:      587,
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password:  ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
+    user_name: ENV["MANDRILL_USERNAME"],
+    password:  ENV["MANDRILL_APIKEY"],
+    authentication: 'login',
     enable_starttls_auto: true
   }
   config.action_mailer.default_url_options = { host: 'www.clarat.org' }

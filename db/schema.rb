7761756b2cfe853ba7e5d853bd6c2936096db4ff
Konstantin K <konstantin.koss@gmail.com>
2016-04-07 11:56:51 +0200

    for #753 - update seeds to include translated main category names

--- db/schema.rb
+++ db/schema.rb
@@ -11,35 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229141529) do
+ActiveRecord::Schema.define(version: 20160321120917) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "areas", force: true do |t|
-    t.string   "name",       limit: nil, null: false
-    t.float    "minlat",                 null: false
-    t.float    "maxlat",                 null: false
-    t.float    "minlong",                null: false
-    t.float    "maxlong",                null: false
+    t.string   "name",       null: false
+    t.float    "minlat",     null: false
+    t.float    "maxlat",     null: false
+    t.float    "minlong",    null: false
+    t.float    "maxlong",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  create_table "base_offers", force: true do |t|
+    t.string "name"
+  end
+
   create_table "categories", force: true do |t|
-    t.string   "name_de",    limit: nil,                null: false
+    t.string   "name_de",                              null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "icon",       limit: 12
     t.integer  "parent_id"
     t.integer  "sort_order"
-    t.boolean  "visible",                default: true
-    t.string   "name_en",    limit: nil
-    t.string   "name_ar",    limit: nil
-    t.string   "name_fr",    limit: nil
-    t.string   "name_pl",    limit: nil
-    t.string   "name_tr",    limit: nil
-    t.string   "name_ru",    limit: nil
+    t.boolean  "visible",               default: true
+    t.string   "name_en"
+    t.string   "name_ar"
+    t.string   "name_fr"
+    t.string   "name_pl"
+    t.string   "name_tr"
+    t.string   "name_ru"
   end
 
   add_index "categories", ["name_de"], name: "index_categories_on_name_de", using: :btree
@@ -70,7 +74,7 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "category_hierarchies", ["descendant_id"], name: "category_desc_idx", using: :btree
 
   create_table "contact_people", force: true do |t|
-    t.integer  "organization_id",                              null: false
+    t.integer  "organization_id",                             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "area_code_1",      limit: 6
@@ -79,15 +83,15 @@ ActiveRecord::Schema.define(version: 20160229141529) do
     t.string   "local_number_2",   limit: 32
     t.string   "fax_area_code",    limit: 6
     t.string   "fax_number",       limit: 32
-    t.string   "first_name",       limit: nil
-    t.string   "last_name",        limit: nil
-    t.string   "operational_name", limit: nil
-    t.string   "academic_title",   limit: nil
-    t.string   "gender",           limit: nil
-    t.string   "responsibility",   limit: nil
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "operational_name"
+    t.string   "academic_title"
+    t.string   "gender"
+    t.string   "responsibility"
     t.integer  "email_id"
-    t.boolean  "spoc",                         default: false, null: false
-    t.string   "position",         limit: nil
+    t.boolean  "spoc",                        default: false, null: false
+    t.string   "position"
   end
 
   add_index "contact_people", ["email_id"], name: "index_contact_people_on_email_id", using: :btree
@@ -102,8 +106,8 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "contact_person_offers", ["offer_id"], name: "index_contact_person_offers_on_offer_id", using: :btree
 
   create_table "contacts", force: true do |t|
-    t.string   "name",       limit: nil
-    t.string   "email",      limit: nil
+    t.string   "name"
+    t.string   "email"
     t.text     "message"
     t.string   "url",        limit: 1000
     t.datetime "created_at"
@@ -126,17 +130,17 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   end
 
   create_table "federal_states", force: true do |t|
-    t.string   "name",       limit: nil, null: false
+    t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "filters", force: true do |t|
-    t.string   "name",              limit: nil, null: false
-    t.string   "identifier",        limit: 35,  null: false
+    t.string   "name",                         null: false
+    t.string   "identifier",        limit: 35, null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "type",              limit: nil, null: false
+    t.string   "type",                         null: false
     t.integer  "section_filter_id"
   end
 
@@ -160,7 +164,7 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "hyperlinks", ["website_id"], name: "index_hyperlinks_on_website_id", using: :btree
 
   create_table "keywords", force: true do |t|
-    t.string "name",     limit: nil
+    t.string "name"
     t.text   "synonyms"
   end
 
@@ -173,24 +177,21 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "keywords_offers", ["offer_id"], name: "index_keywords_offers_on_offer_id", using: :btree
 
   create_table "locations", force: true do |t|
-    t.string   "street",           limit: nil,                null: false
+    t.string   "street",                          null: false
     t.text     "addition"
-    t.string   "zip",              limit: nil,                null: false
-    t.string   "city",             limit: nil,                null: false
+    t.string   "zip",                             null: false
+    t.string   "city",                            null: false
     t.boolean  "hq"
     t.float    "latitude"
     t.float    "longitude"
-    t.integer  "organization_id",                             null: false
-    t.integer  "federal_state_id",                            null: false
+    t.integer  "organization_id",                 null: false
+    t.integer  "federal_state_id",                null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",             limit: nil
-    t.string   "display_name",     limit: nil,                null: false
-    t.string   "area_code",        limit: 6
-    t.string   "local_number",     limit: 32
-    t.string   "email",            limit: nil
-    t.boolean  "visible",                      default: true
-    t.boolean  "in_germany",                   default: true
+    t.string   "name"
+    t.string   "display_name",                    null: false
+    t.boolean  "visible",          default: true
+    t.boolean  "in_germany",       default: true
   end
 
   add_index "locations", ["created_at"], name: "index_locations_on_created_at", using: :btree
@@ -199,17 +200,17 @@ ActiveRecord::Schema.define(version: 20160229141529) do
 
   create_table "logic_versions", force: true do |t|
     t.integer "version"
-    t.string  "name",    limit: nil
+    t.string  "name"
   end
 
   create_table "next_steps", force: true do |t|
-    t.string "text_de", limit: nil, null: false
-    t.string "text_en", limit: nil
-    t.string "text_ar", limit: nil
-    t.string "text_fr", limit: nil
-    t.string "text_pl", limit: nil
-    t.string "text_tr", limit: nil
-    t.string "text_ru", limit: nil
+    t.string "text_de", null: false
+    t.string "text_en"
+    t.string "text_ar"
+    t.string "text_fr"
+    t.string "text_pl"
+    t.string "text_tr"
+    t.string "text_ru"
   end
 
   add_index "next_steps", ["text_de"], name: "index_next_steps_on_text_de", using: :btree
@@ -252,8 +253,8 @@ ActiveRecord::Schema.define(version: 20160229141529) do
 
   create_table "offer_translations", force: true do |t|
     t.integer  "offer_id",                                       null: false
-    t.string   "locale",                limit: nil,              null: false
-    t.string   "source",                limit: nil, default: "", null: false
+    t.string   "locale",                                         null: false
+    t.string   "source",                            default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name",                  limit: 120, default: "", null: false
@@ -269,8 +270,8 @@ ActiveRecord::Schema.define(version: 20160229141529) do
     t.string   "name",                        limit: 120,                 null: false
     t.text     "description",                                             null: false
     t.text     "old_next_steps"
-    t.string   "encounter",                   limit: nil
-    t.string   "slug",                        limit: nil
+    t.string   "encounter"
+    t.string   "slug"
     t.integer  "location_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -284,25 +285,27 @@ ActiveRecord::Schema.define(version: 20160229141529) do
     t.text     "description_html"
     t.text     "next_steps_html"
     t.text     "opening_specification_html"
-    t.string   "exclusive_gender",            limit: nil
+    t.string   "exclusive_gender"
     t.integer  "age_from",                                default: 0
     t.integer  "age_to",                                  default: 99
-    t.string   "target_audience",             limit: nil
+    t.string   "target_audience"
     t.string   "aasm_state",                  limit: 32
     t.boolean  "hide_contact_people",                     default: false
     t.boolean  "age_visible",                             default: false
     t.string   "code_word",                   limit: 140
     t.integer  "solution_category_id"
-    t.string   "treatment_type",              limit: nil
-    t.string   "participant_structure",       limit: nil
-    t.string   "gender_first_part_of_stamp",  limit: nil
-    t.string   "gender_second_part_of_stamp", limit: nil
+    t.string   "treatment_type"
+    t.string   "participant_structure"
+    t.string   "gender_first_part_of_stamp"
+    t.string   "gender_second_part_of_stamp"
     t.integer  "logic_version_id"
+    t.integer  "base_offer_id"
   end
 
   add_index "offers", ["aasm_state"], name: "index_offers_on_aasm_state", using: :btree
   add_index "offers", ["approved_at"], name: "index_offers_on_approved_at", using: :btree
   add_index "offers", ["area_id"], name: "index_offers_on_area_id", using: :btree
+  add_index "offers", ["base_offer_id"], name: "index_offers_on_base_offer_id", using: :btree
   add_index "offers", ["created_at"], name: "index_offers_on_created_at", using: :btree
   add_index "offers", ["location_id"], name: "index_offers_on_location_id", using: :btree
   add_index "offers", ["logic_version_id"], name: "index_offers_on_logic_version_id", using: :btree
@@ -317,13 +320,13 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "offers_openings", ["opening_id"], name: "index_offers_openings_on_opening_id", using: :btree
 
   create_table "openings", force: true do |t|
-    t.string   "day",        limit: 3,   null: false
+    t.string   "day",        limit: 3, null: false
     t.time     "open"
     t.time     "close"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "sort_value"
-    t.string   "name",       limit: nil, null: false
+    t.string   "name",                 null: false
   end
 
   add_index "openings", ["day"], name: "index_openings_on_day", using: :btree
@@ -338,35 +341,35 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "organization_offers", ["organization_id"], name: "index_organization_offers_on_organization_id", using: :btree
 
   create_table "organization_translations", force: true do |t|
-    t.integer  "organization_id",                          null: false
-    t.string   "locale",          limit: nil,              null: false
-    t.string   "source",          limit: nil, default: "", null: false
+    t.integer  "organization_id",              null: false
+    t.string   "locale",                       null: false
+    t.string   "source",          default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "description",                 default: "", null: false
+    t.text     "description",     default: "", null: false
   end
 
   add_index "organization_translations", ["locale"], name: "index_organization_translations_on_locale", using: :btree
   add_index "organization_translations", ["organization_id"], name: "index_organization_translations_on_organization_id", using: :btree
 
   create_table "organizations", force: true do |t|
-    t.string   "name",                   limit: nil,                 null: false
-    t.text     "description",                                        null: false
-    t.string   "legal_form",             limit: nil,                 null: false
-    t.boolean  "charitable",                         default: false
+    t.string   "name",                                              null: false
+    t.text     "description",                                       null: false
+    t.string   "legal_form",                                        null: false
+    t.boolean  "charitable",                        default: false
     t.integer  "founded"
     t.string   "umbrella",               limit: 8
-    t.string   "slug",                   limit: nil
+    t.string   "slug"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "approved_at"
-    t.integer  "offers_count",                       default: 0
-    t.integer  "locations_count",                    default: 0
+    t.integer  "offers_count",                      default: 0
+    t.integer  "locations_count",                   default: 0
     t.integer  "created_by"
     t.integer  "approved_by"
-    t.boolean  "accredited_institution",             default: false
+    t.boolean  "accredited_institution",            default: false
     t.text     "description_html"
-    t.boolean  "mailings_enabled",                   default: false
+    t.boolean  "mailings_enabled",                  default: false
     t.string   "aasm_state",             limit: 32
   end
 
@@ -375,28 +378,28 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "organizations", ["created_at"], name: "index_organizations_on_created_at", using: :btree
 
   create_table "search_locations", force: true do |t|
-    t.string   "query",      limit: nil, null: false
-    t.float    "latitude",               null: false
-    t.float    "longitude",              null: false
+    t.string   "query",                 null: false
+    t.float    "latitude",              null: false
+    t.float    "longitude",             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "geoloc",     limit: 35,  null: false
+    t.string   "geoloc",     limit: 35, null: false
   end
 
   add_index "search_locations", ["geoloc"], name: "index_search_locations_on_geoloc", using: :btree
   add_index "search_locations", ["query"], name: "index_search_locations_on_query", using: :btree
 
   create_table "sitemaps", force: true do |t|
-    t.string "path",    limit: nil, null: false
+    t.string "path",    null: false
     t.text   "content"
   end
 
   add_index "sitemaps", ["path"], name: "index_sitemaps_on_path", unique: true, using: :btree
 
   create_table "solution_categories", force: true do |t|
-    t.string   "name",       limit: nil
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "parent_id"
   end
 
@@ -419,38 +422,38 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "statistics", ["user_id"], name: "index_statistics_on_user_id", using: :btree
 
   create_table "subscriptions", force: true do |t|
-    t.string   "email",      limit: nil
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "update_requests", force: true do |t|
-    t.string   "search_location", limit: nil, null: false
-    t.string   "email",           limit: nil, null: false
+    t.string   "search_location", null: false
+    t.string   "email",           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",              limit: nil, default: "",         null: false
-    t.string   "encrypted_password", limit: nil, default: "",         null: false
+    t.string   "email",              default: "",         null: false
+    t.string   "encrypted_password", default: "",         null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "role",               limit: nil, default: "standard"
-    t.integer  "failed_attempts",                default: 0,          null: false
+    t.string   "role",               default: "standard"
+    t.integer  "failed_attempts",    default: 0,          null: false
     t.datetime "locked_at"
-    t.string   "provider",           limit: nil
-    t.string   "uid",                limit: nil
-    t.string   "name",               limit: nil
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "versions", force: true do |t|
-    t.string   "item_type",      limit: nil, null: false
-    t.integer  "item_id",                    null: false
-    t.string   "event",          limit: nil, null: false
-    t.string   "whodunnit",      limit: nil
+    t.string   "item_type",      null: false
+    t.integer  "item_id",        null: false
+    t.string   "event",          null: false
+    t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
     t.text     "object_changes"
@@ -459,8 +462,8 @@ ActiveRecord::Schema.define(version: 20160229141529) do
   add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
   create_table "websites", force: true do |t|
-    t.string   "host",       limit: nil, null: false
-    t.string   "url",        limit: nil, null: false
+    t.string   "host",       null: false
+    t.string   "url",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end

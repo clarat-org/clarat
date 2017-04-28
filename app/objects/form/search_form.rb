dd4eedab33faae9492383ac1d953e87a3390bd63
mokus80 <felhoesi@clarat.org>
2017-04-28 13:09:41 +0200

    add geolocation to search form inititalizer

--- app/objects/form/search_form.rb
+++ app/objects/form/search_form.rb
@@ -56,6 +56,8 @@ class SearchForm
     return if exact_location
     if search_location.blank? # Blank location => use cookies or default fallback
       load_geolocation_values!(cookies)
+    elsif search_location && generated_geolocation # if geolocation has been set, use it!
+      generated_geolocation
     elsif search_location && search_location != I18n.t('conf.current_location')
       self.generated_geolocation = search_location_instance.geoloc
     end

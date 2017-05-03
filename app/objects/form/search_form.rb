48ab995481481f20eac1019f6ec77b74bdd6b3ab
mokus80 <felhoesi@clarat.org>
2017-05-03 16:43:43 +0200

    please rubocop

--- app/objects/form/search_form.rb
+++ app/objects/form/search_form.rb
@@ -52,12 +52,12 @@ class SearchForm
 
   def initialize cookies, *attrs
     super(*attrs)
-    current_location_list = %i[ar de en fa fr pl ru tr].map {|t| I18n.backend.send(:translations)[t][:conf][:current_location]}
+    current_location_list = %i[ar de en fa fr pl ru tr].map { |t| I18n.backend.send(:translations)[t][:conf][:current_location] }
     return if exact_location
 
     if search_location.blank? # Blank location => use cookies or default fallback
       load_geolocation_values!(cookies)
-    elsif (current_location_list.include? search_location ) && generated_geolocation.present? # if geolocation has been set, use it!
+    elsif (current_location_list.include? search_location) && generated_geolocation.present? # if geolocation has been set, use it!
       generated_geolocation
     elsif search_location && search_location != I18n.t('conf.current_location')
       self.generated_geolocation = search_location_instance.geoloc

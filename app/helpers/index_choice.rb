90043281506fbd02f28cddd93023029ba850ce27
mokus80 <felhoesi@clarat.org>
2017-08-21 16:45:50 +0200

    remove popup on refugee page

--- app/helpers/index_choice.rb
+++ app/helpers/index_choice.rb
@@ -4,7 +4,7 @@ module IndexChoice
     if Rails.application.secrets.force_production_index
       "Offer_production_personal_#{I18n.locale}"
     else
-      Offer.personal_index_name(I18n.locale)
+      "Offer_production_personal_#{I18n.locale}"
     end
   end
 
@@ -12,7 +12,7 @@ module IndexChoice
     if Rails.application.secrets.force_production_index
       "Offer_production_remote_#{I18n.locale}"
     else
-      Offer.remote_index_name(I18n.locale)
+      "Offer_production_remote_#{I18n.locale}"
     end
   end
 end

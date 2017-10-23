8a08993be47feee90e7ded593ee743e8d2280ede
Konstantin K <konstantin.koss@gmail.com>
2017-10-23 15:25:22 +0200

    fix for last commit: I18n.l not needed anymore for opening

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -8,8 +8,7 @@ class Opening < ActiveRecord::Base
   # The way an opening time frame is displayed to the user in the front end.
   def display_string
     if close
-      close_string = I18n.l(close, format:
-        I18n.t('opening.display_string.time_format_close'))
+      close_string = close
       # midnight exception for de format. Can be expanded to other languages
       # by checking close.hour and close.minutes and adding content in locales
       if close_string == '00:00 Uhr'
@@ -26,10 +25,7 @@ class Opening < ActiveRecord::Base
   def time_frame open, close_string
     I18n.t(
       'opening.display_string.time_frame',
-      open: I18n.l(
-        open,
-        format: I18n.t('opening.display_string.time_format_open')
-      ),
+      open: open,
       close: close_string
     )
   end

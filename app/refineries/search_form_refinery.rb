c0e52580e32a364ad63d6061fd7f5a945d74a019
allthegoldt <damjangoldt@Damjans-MacBook-Air.local>
2017-11-03 13:37:31 +0100

    added tag factory

--- app/refineries/search_form_refinery.rb
+++ app/refineries/search_form_refinery.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
+
 class SearchFormRefinery < ApplicationRefinery
   def root
     'search_form'
   end
 
   def default
-    [
-      :query, :generated_geolocation, :search_location, :language,
-      :exact_location, :contact_type, :target_audience, :exclusive_gender,
-      :residency_status
+    %i[
+      query generated_geolocation search_location language
+      exact_location contact_type target_audience exclusive_gender
+      residency_status
     ]
   end
 end
---
+++ test/factories/tag.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'ffaker'
+
+FactoryGirl.define do
+  factory :tag do
+    name_de { FFaker::Lorem.words(rand(2..3)).join(' ').titleize }
+    name_en { name_de + ' (en)' }
+    keywords_de { FFaker::Lorem.words(rand(2..3)).join(' ').titleize }
+
+    trait :with_dummy_translations do
+      after :create do |tag, _evaluator|
+        (I18n.available_locales - [:de]).each do |locale|
+          tag["name_#{locale}"] = "#{locale}(#{tag.name_de})"
+          tag.save
+        end
+      end
+    end
+  end
+end
